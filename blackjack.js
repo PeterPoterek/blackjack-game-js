cb03e260deef2fdecd6d865fc2d928301f314e41
Piotr Poterek <peterpoterek0@gmail.com>
2023-09-26 23:52:23 +0200

    game loop is now working

--- blackjack.js
+++ blackjack.js
@@ -17,27 +17,20 @@ const generateDeck = () => {
 };
 
 const drawCard = (deck) => {
+  if (deck.length === 0) {
+    console.log("The deck is empty.");
+    return null;
+  }
+
   const randomNumber = Math.floor(Math.random() * deck.length);
-  deck.splice(randomNumber, 1);
-  return deck[randomNumber];
+  const drawnCard = deck.splice(randomNumber, 1)[0];
+
+  return drawnCard;
 };
-
-const deck = generateDeck();
-
-const playerHand = [];
-const dealerHand = [];
-
-playerHand.push(drawCard(deck));
-playerHand.push(drawCard(deck));
-
-dealerHand.push(drawCard(deck));
-dealerHand.push(drawCard(deck));
-
 const checkScore = (hand) => {
   let points = 0;
 
   for (const cardObject of hand) {
-    console.log(cardObject);
     switch (cardObject.card) {
       case "Jack":
       case "Queen":
@@ -53,8 +46,47 @@ const checkScore = (hand) => {
     }
   }
 
-  console.log(points);
+  return points;
 };
+const deck = generateDeck();
 
-checkScore(playerHand);
-checkScore(dealerHand);
+const playerHand = [];
+const dealerHand = [];
+
+let playerScore = 0;
+let dealerScore = 0;
+
+playerHand.push(drawCard(deck));
+playerHand.push(drawCard(deck));
+
+dealerHand.push(drawCard(deck));
+dealerHand.push(drawCard(deck));
+
+while (true) {
+  playerHand.push(drawCard(deck));
+
+  playerScore = checkScore(playerHand);
+  dealerScore = checkScore(dealerHand);
+
+  if (playerScore > 21) {
+    console.log(`You lose! Your final score was ${playerScore} while the dealer had ${dealerScore}`);
+    break;
+  }
+  if (playerScore === 21) {
+    console.log(`You win! Your final score was ${playerScore} while the dealer had ${dealerScore}`);
+    break;
+  }
+
+  dealerHand.push(drawCard(deck));
+  dealerScore = checkScore(dealerHand);
+
+  if (dealerScore > 21) {
+    console.log(`You Win! Your final score was ${playerScore} while the dealer had ${dealerScore}`);
+    break;
+  }
+
+  if (dealerScore === 21) {
+    console.log(`You lose! Your final score was ${playerScore} while the dealer had ${dealerScore}`);
+    break;
+  }
+}
