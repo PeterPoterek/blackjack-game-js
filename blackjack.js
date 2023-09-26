f8f0851799ded3aa5453449256e63d0c8812cf16
Piotr Poterek <peterpoterek0@gmail.com>
2023-09-26 19:54:10 +0200

    generated deck code moved to separate function

--- blackjack.js
+++ blackjack.js
@@ -1,15 +1,20 @@
-const deck = [];
+const generateDeck = () => {
+  const deck = [];
 
-const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
-const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+  const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+  const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
 
-for (const card of cards) {
-  for (const suit of suits) {
-    deck.push({
-      card: card,
-      suit: suit,
-    });
+  for (const card of cards) {
+    for (const suit of suits) {
+      deck.push({
+        card: card,
+        suit: suit,
+      });
+    }
   }
-}
 
+  return deck;
+};
+
+const deck = generateDeck();
 console.log(deck);
